4358ee85b1789ffb49094e87884585304077fea6
Primdahl26 <frederik.vincent@hotmail.com>
2021-03-06 11:56:50 +0100

    Added new html sites for search and main, and updated the code to use them

--- app/api.py
+++ app/api.py
@@ -5,9 +5,8 @@ import requests
 api_key = config('THE_MOVIE_DB_API_KEY')
 language_url = '&language=en-DK'
 
-
+#TODO: Make images with ? either find a image or remove them from the movie list
 def get_movie_search_list(keyword):
-     
     search_api_url = f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={keyword}' + language_url
     data = requests.get(search_api_url).json()
 
@@ -38,9 +37,8 @@ def get_movie_search_list(keyword):
 
 
 # TODO: Make it support more than danish & more than flatrate
-# Right now it only gets danish providers & it only gets flatrate options (streaming as netflix)
+# Right now it only gets danish providers & it only gets flatrate options (streaming as netflix, HBO, Viaplay and so on )
 def get_movie_provider_list(movie_id):
-
     search_api_url = f'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers?api_key={api_key}'+ language_url
     data = requests.get(search_api_url).json()
 
@@ -58,8 +56,8 @@ def get_movie_provider_list(movie_id):
 
     return provider_list
 
-def get_movie_from_id(movie_id):
 
+def get_movie_from_id(movie_id):
     search_api_url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}'+ language_url
     data = requests.get(search_api_url).json()
 
@@ -78,38 +76,38 @@ def get_movie_from_id(movie_id):
 
     return movie_data
 
-#TODO: Find a way to get more page numbers in the movie list
-def get_top_rated_movies_list(page):
 
+def get_movie_list(page, type):
     page_url = '&page=' + str(page)
 
-    search_api_url = f'https://api.themoviedb.org/3/movie/top_rated?api_key={api_key}'+ language_url + page_url
+    search_api_url = f'https://api.themoviedb.org/3/movie/{type}?api_key={api_key}'+ language_url + page_url
     data = requests.get(search_api_url).json()
 
-    top_rated_movie_list = []
+    movie_list = []
 
     for element in data['results']:
         # If the poster image os none, and there is no release_date,
         # then we use Unknown as release_date, and a ? image for the poster
         if element['poster_path'] is None and 'release_date' not in element:
             temp_dict = {'id' : element['id'], 'title': element['title'], 'release_date': 'Unknown', 'overview' : element['overview'] ,'poster_path': 'https://upload.wikimedia.org/wikipedia/commons/2/2f/QuestionMark.jpg'}
-            top_rated_movie_list.append(temp_dict)
+            movie_list.append(temp_dict)
         # If there is no release date,
         # then we use Unknown as release_date
         elif 'release_date' not in element:
             temp_dict = {'id' : element['id'], 'title': element['title'], 'release_date': 'Unknown', 'overview' : element['overview'] ,'poster_path': 'https://image.tmdb.org/t/p/w500/' + element['poster_path']}
-            top_rated_movie_list.append(temp_dict)
+            movie_list.append(temp_dict)
         # If the poster image is none,
         # then we use a ? image for the poster
         elif element['poster_path'] is None:
             temp_dict = {'id' : element['id'], 'title': element['title'], 'release_date': element['release_date'], 'overview' : element['overview'] ,'poster_path': 'https://upload.wikimedia.org/wikipedia/commons/2/2f/QuestionMark.jpg'}
-            top_rated_movie_list.append(temp_dict)
+            movie_list.append(temp_dict)
         # Else all the information we need is there
         else:
             temp_dict = {'id' : element['id'], 'title': element['title'], 'release_date': element['release_date'], 'overview' : element['overview'] ,'poster_path': 'https://image.tmdb.org/t/p/w500/' + element['poster_path']}
-            top_rated_movie_list.append(temp_dict)
+            movie_list.append(temp_dict)
 
-    return top_rated_movie_list
+    return movie_list
+
 
 # Takes a list of movies, and returns only the movies that are streamable
 # on the provider in the parameter
@@ -128,6 +126,7 @@ def get_movie_list_specific_provider(movie_list, provider):
     return specific_movie_list
 
 
+# TODO: Finish 
 def get_genres(movie_list):
     search_api_url = f'https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}'+ language_url
     data = requests.get(search_api_url).json()
--- app/app.py
+++ app/app.py
@@ -16,53 +16,63 @@ from .api import (
     get_movie_search_list, 
     get_movie_provider_list, 
     get_movie_from_id, 
-    get_top_rated_movies_list,
     get_movie_list_specific_provider,
-    get_genres
+    get_genres,
+    get_movie_list
 )
 
 main = Blueprint("main", __name__)
 
 # TODO: Find way to not use global variables
-top_rated_movie_page = 1
 provider = 'Any'
 
 @main.route('/', methods=['POST', 'GET'])
 def index():
 
     global provider
-    global top_rated_movie_page
+    page = 1 
 
     if request.method == 'GET':
-        top_rated_movie_page = 1
+        provider = 'Any'
+        page = 1
 
-    top_rated_list = get_top_rated_movies_list(top_rated_movie_page)
+    top_rated_list = get_movie_list(page, 'top_rated')
+    top_rated_list.extend(get_movie_list(page+1, 'top_rated'))
+    print('Top rated list size:')
+    print(len(top_rated_list))
+
+    popular_list = get_movie_list(page, 'popular')
+    popular_list.extend(get_movie_list(page+1, 'popular'))
+    print('Popular list size:')
+    print(len(popular_list))
+
+    upcoming_list = get_movie_list(page, 'upcoming')
+    upcoming_list.extend(get_movie_list(page+1, 'upcoming'))
+    print('Upcoming list size:')
+    print(len(upcoming_list))
+
 
     if request.method == 'POST':
-        if 'Next' in request.form:
-            top_rated_movie_page += 1
-            top_rated_list = get_top_rated_movies_list(top_rated_movie_page)
-
-            if provider != 'Any':
-                top_rated_list = get_movie_list_specific_provider(top_rated_list, provider)
-
-        elif 'Previous' in request.form:
-            top_rated_movie_page = top_rated_movie_page - 1
-            top_rated_list = get_top_rated_movies_list(top_rated_movie_page)
-            
-            if provider != 'Any':
-                top_rated_list = get_movie_list_specific_provider(top_rated_list, provider)
-
-        elif 'Filter' in request.form:
+        if 'Filter' in request.form:
             provider = request.form['Provider']
 
             if provider == 'Any':
-                top_rated_list = get_top_rated_movies_list(top_rated_movie_page)
+                top_rated_list = get_movie_list(page, 'top_rated')
+                top_rated_list.extend(get_movie_list(page+1, 'top_rated'))
+
+                popular_list = get_movie_list(page, 'popular')
+                popular_list.extend(get_movie_list(page+1, 'popular'))
+
+                upcoming_list = get_movie_list(page, 'upcoming')
+                upcoming_list.extend(get_movie_list(page+1, 'upcoming'))
+
             else:
                 top_rated_list = get_movie_list_specific_provider(top_rated_list, provider)
+                popular_list = get_movie_list_specific_provider(popular_list, provider)
+                upcoming_list = get_movie_list_specific_provider(upcoming_list, provider)
         
 
-    return render_template('index.html', top_rated_list=top_rated_list, page_number=top_rated_movie_page, provider=provider)
+    return render_template('main.html', top_rated_movies=top_rated_list, popular_movies=popular_list, upcoming_movies=upcoming_list)
 
 
 @main.route('/movie/<int:movie_id>', methods=['POST', 'GET'])
@@ -99,7 +109,7 @@ def search():
     elif provider != 'Any':
         movie_list = get_movie_list_specific_provider(movie_list, provider)
 
-    return render_template('search.html', movie_list=movie_list, keyword=result, provider=provider)
+    return render_template('search2.html', movie_list=movie_list, keyword=result, provider=provider)
     
     if request.method == 'GET':
         return redirect('/')
